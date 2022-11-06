b7bf5c3c4d23258c2e3e4b86b20ab290e8a405e0
Chemmy-Jack <kuojack0114@gmail.com>
2022-11-06 20:58:33 +0800

    try fix float bug on pi3, indent error

--- src/nturt_push_to_control_tower_core.cpp
+++ src/nturt_push_to_control_tower_core.cpp
@@ -15,27 +15,32 @@ int P2ctower_core::push2_ctower(std::string name, double value, double time) {
 };
 
 int P2ctower_core::init_websocket(std::string host, std::string port){
-    host_ = host;
-    port_ = port;
-    // tcp::resolver resolvger_{ioc_};
-    std::cout << "[debug]:tcp::resolver resolver_{ioc_};" << std::endl;
-    auto const results = resolver_.resolve(host_, port_);
-    std::cout << "[debug]:auto const results = resolver_.resolve(host_, port_);" << std::endl ;
-    ep_ = net::connect(ws_.next_layer(), results);
-    std::cout << "[debug]:ep_ = net::connect(ws_.next_layer(), results);" << std::endl ;
-    host_ += ':' + std::to_string(ep_.port());
-    ws_.set_option(websocket::stream_base::decorator(
-        [](websocket::request_type& req)
-        {
-            req.set(http::field::user_agent,
-                std::string(BOOST_BEAST_VERSION_STRING) +
-                    " websocket-client-coro");
-        }));
-    std::cout << "[debug]:set_option" << std::endl ;
-    ws_.handshake(host_, "/");
-    std::cout << "[debug]:ws_.handshake(host_, '');" << std::endl ;
-    ws_.write(net::buffer(std::string("Successfully init webocket")));
-    return OK;
+    try {
+        host_ = host;
+        port_ = port;
+        // tcp::resolver resolvger_{ioc_};
+        std::cout << "[debug]:tcp::resolver resolver_{ioc_};" << std::endl;
+        auto const results = resolver_.resolve(host_, port_);
+        std::cout << "[debug]:auto const results = resolver_.resolve(host_, port_);" << std::endl ;
+        ep_ = net::connect(ws_.next_layer(), results);
+        std::cout << "[debug]:ep_ = net::connect(ws_.next_layer(), results);" << std::endl ;
+        host_ += ':' + std::to_string(ep_.port());
+        ws_.set_option(websocket::stream_base::decorator(
+            [](websocket::request_type& req)
+            {
+                req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-coro");
+            }));
+        std::cout << "[debug]:set_option" << std::endl ;
+        ws_.handshake(host_, "/");
+        std::cout << "[debug]:ws_.handshake(host_, '');" << std::endl ;
+        ws_.write(net::buffer(std::string("Successfully init webocket")));
+        return OK;
+    } catch (error) {
+        std::cout << "init websocket fail" << std::endl;
+        return ERR;
+    }
 };
 
 
